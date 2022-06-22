735af526f9fd02c19c51c74c3899933ec4a18ff8
unknown <suntanabhagyashree@gmail.com>
2022-06-22 15:57:13 +0530

    ADDED:Calculating Daily Employe Wage

--- src/EmployeWage.java
+++ src/EmployeWage.java
@@ -1,12 +1,18 @@
 public class EmployeWage {
     public static void main(String[] args) {
-        int IS_FULL_TIME = 1;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_FULL_TIME)
-            System.out.println("Employee is Present");
-        else
-            System.out.println("Employee is absent");
-    }
-}
 
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            double empCheck = Math.floor(Math.random() * 10) % 2;
+            if (empCheck == IS_FULL_TIME)
+                empHrs = 8;
+            else
+                empHrs = 0;
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            System.out.println("Emp Wage: " + empWage);
+
+        }
+}
 
